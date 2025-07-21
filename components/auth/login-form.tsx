b4edbfecd67ce6d4bd0459c02cbd74ab911f5468
ibrahimbajwa313 <126707772+Mimran-ship-it@users.noreply.github.com>
2025-07-21 11:48:35 +0500

    form updated

--- components/auth/login-form.tsx
+++ components/auth/login-form.tsx
@@ -63,7 +63,7 @@ export function LoginForm() {
   }
 
   return (
-    <form onSubmit={handleSubmit} className="space-y-4">
+    <form onSubmit={handleSubmit} className="space-y-4 ">
       <div className="space-y-2">
         <Label htmlFor="email">Email</Label>
         <Input
