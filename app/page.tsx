9ddd4386e78525b0d92d4a865ab6597038a0f716
ibrahimbajwa313 <126707772+Mimran-ship-it@users.noreply.github.com>
2025-08-03 13:42:15 +0500

    bestseller section added

--- app/page.tsx
+++ app/page.tsx
@@ -5,6 +5,7 @@ import { TrendingProducts } from "@/components/trending-products"
 import { EveryonesFavourite } from "@/components/everyone's-Favourite"
 import { CategorySection } from "@/components/category-section"
 import { FAQSection } from "@/components/faq-section"
+import BestSellingSection from "@/components/BestSellingSection"
 import { TopBanner } from "@/components/top_banner"
 import {WhatsappReviewSection} from "@/components/reviews"
 // app/layout.js or pages/_app.js
@@ -23,6 +24,7 @@ export default function HomePage() {
 
         <TrendingProducts /> 
         <CategorySection />
+        <BestSellingSection />
         <WhatsappReviewSection/>
       </main>
       <FAQSection/>
